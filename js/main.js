df2d99cb18c92e8a744ab303ef0589f0683f083a
Matt Pedersen <mpedersen2054@gmail.com>
2014-11-05 21:53:31 -0600

    add jquery to allow the changing of a tasks state

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 
 $(document).ready(function () {
 
+  // add text from input and other functionality
   $('#task').on('keydown', function (e) {
 
     if (e.which == 13) {
       var value = $(this).val();
-      $('.task-list').append('<li class="task"><h4> '+value+'</h4></li>');
+      $('.task-list').append('<li class="task"><p> '+value+'</p></li>');
       $(this).val('');
     }
 
@@ -23,14 +24,30 @@ $(document).ready(function () {
 
   });
 
+  // add task from textarea
   $('#large-task-button').on('mousedown', function () {
     var value = $('#large-task').val();
-    $('.task-list').append('<li class="task"><h4> '+value+'</h4></li>');
+    $('.task-list').append('<li class="task"><p> '+value+'</p></li>');
   })
   .on('mouseup', function () {
     $('#large-task-button').hide();
     $('#large-task').hide();
     $('#task').show().val('').focus();
+  });
+
+  // toggle task completion state
+  $('.task-list').on('click', '.task', function () {
+
+    if ($(this).is('.complete')) {
+      $(this).removeClass('complete');
+      $(this).addClass('incomplete');
+      $(this).find('.state').text('incomplete');
+    } else {
+      $(this).removeClass('incomplete');
+      $(this).addClass('complete');
+      $(this).find('.state').text('complete');
+    }
+
   });
 
 });
