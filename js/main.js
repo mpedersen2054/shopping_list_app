2fdb873e112024bcee2c5c3b371f4f327d612479
Matt Pedersen <mpedersen2054@gmail.com>
2014-12-29 16:47:06 -0600

    refactor js

--- js/main.js
+++ js/main.js
@@ -1,60 +1,67 @@
-
-$(document).ready(function () {
+(function($, document) {
+  var $task = $('#task'),
+      $noTaskNotice = $('.no-task-notice'),
+      $taskList = $('.task-list'),
+      $largeTask = $('#large-task'),
+      $largeTaskBtn = ('#large-task-button');
 
   // add text from input and other keyboard functionality
-  $('#task').on('keydown', function (e) {
+  $task.on('keydown', function (e) {
+    var $this = $(this);
 
-    if (e.which == 13) {
-      var value = $(this).val();
-      $('.no-task-notice').hide();
-      $('.task-list').prepend('<li class="task incomplete"><p> '+value+'</p><span class="state">incomplete</span></li>');
-      $(this).val('');
+    if (e.which === 13) {
+      var value = $this.val();
+      $noTaskNotice.hide();
+      $taskList.prepend('<li class="task incomplete"><p> '+value+'</p><span class="state">incomplete</span></li>');
+      $this.val('');
     }
 
-    if (e.which == 27) {
-      $(this).val('');
+    if (e.which === 27) {
+      $this.val('');
     }
 
-    if (e.which == 40) {
+    if (e.which === 40) {
       e.preventDefault();
-      var value = $(this).val();
-      $(this).hide();
-      $('#large-task').show().val(value).focus();
-      $('#large-task-button').show();
+      var value = $this.val();
+      $this.hide();
+      $largeTask.show().val(value).focus();
+      $largeTaskBtn.show();
     }
 
   });
 
   // add task from textarea
   $('#large-task-button').on('mousedown', function () {
-    var value = $('#large-task').val();
-    $('.no-task-notice').hide();
-    $('.task-list').prepend('<li class="task incomplete"><p> '+value+'</p><span class="state">incomplete</span></li>');
+    var value = $largeTask.val();
+
+    $noTaskNotice.hide();
+    $taskList.prepend('<li class="task incomplete"><p> '+value+'</p><span class="state">incomplete</span></li>');
   })
   .on('mouseup', function () {
-    $('#large-task-button').hide();
-    $('#large-task').hide();
-    $('#task').show().val('').focus();
+    $largeTaskBtn.hide();
+    $largeTask.hide();
+    $task.show().val('').focus();
   });
 
   // toggle task completion state
-  $('.task-list').on('click', '.task', function () {
+  $taskList.on('click', '.task', function () {
+    var $this = $(this);
 
     // toggle to incomplete
-    if ($(this).is('.complete')) {
-      $(this).removeClass('complete');
-      $(this).addClass('incomplete');
-      $(this).find('.state').show().text('incomplete');
-      $('.task-list').prepend($(this));
-      $(this).css('opacity', '1.0');
+    if ($this.is('.complete')) {
+      $this.removeClass('complete');
+      $this.addClass('incomplete');
+      $this.find('.state').show().text('incomplete');
+      $taskList.prepend($this);
+      $this.css('opacity', '1.0');
 
     // toggle to complete
     } else {
-      $(this).removeClass('incomplete');
-      $(this).addClass('complete');
-      $(this).find('.state').hide();
-      $('.task-list').append($(this));
-      $(this).css('opacity', '0.5');
+      $this.removeClass('incomplete');
+      $this.addClass('complete');
+      $this.find('.state').hide();
+      $taskList.append($this);
+      $this.css('opacity', '0.5');
     }
 
   });
@@ -63,14 +70,15 @@ $(document).ready(function () {
   // this probably isnt need since there is
   // no persistence
   $('.task').each(function (i) {
-    if ($(this).is('.complete')) {
-      $(this).find('.state').text('');
-      $('.task-list').append($(this));
-      $(this).css('opacity', '0.5');
+    var $this = $(this);
+
+    if ($this.is('.complete')) {
+      $this.find('.state').text('');
+      $taskList.append($this);
+      $this.css('opacity', '0.5');
     } else {
-      $(this).find('.state').text('incomplete');
-      $('.task-list').prepend($(this));
+      $this.find('.state').text('incomplete');
+      $taskList.prepend($this);
     }
   })
-
-});
+})(jQuery, document);
